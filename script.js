6da60199eb0dd23895c39fe994d860049f0dad52
Sahnwaz786 <mdsahnwazalam48@gmail.com>
2023-11-25 14:47:10 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,326 @@
+gsap.from("#page1 #part1",{
+    dealy:4,
+    opacity:0,
+    duration:2,
+    x:-300
+
+})
+gsap.from("#page1 #part2",{
+    dealy:4,
+    opacity:0,
+    duration:2,
+    y:300
+
+})
+gsap.from("#page1 #part3",{
+    dealy:4,
+    opacity:0,
+    duration:2,
+    x:300
+
+})
+gsap.from("#page2 #part-1",{
+   opacity:0,
+    duration:2,
+    dealy:1,
+    x:-400,
+    scrollTrigger:{
+        trigger:"#page2 #part-1",
+        scroller:"body",
+        // markers:true,
+        start:"top 70%",
+        end :"bottom 10%",
+      
+    } })
+    
+gsap.from("#page2 #part-2",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     x:400,
+     scrollTrigger:{
+         trigger:"#page2 #part-2",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+        
+gsap.from("#page-3 #part1 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page-3 #part1",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page-3 #part2",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page-3 #part2",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page4 #part1 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page4 #part1",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page4 #part2 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page4 #part2",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page3 #starter ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page3 #starter",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page3 #vape ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page3 #vape",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page3 #tanks ",{
+    opacity:0,
+     duration:1.5,
+     dealy:1,
+     x:300,
+     scrollTrigger:{
+         trigger:"#page3 #tanks",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page3 #acc ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page3 #acc",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page3 #e ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page3 #e",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page5 #part1 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page5 #part1",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page5 #part2 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page5 #part2",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page5 #part3 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page5 #part3",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page6 #part-1 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     x:-300,
+     scrollTrigger:{
+         trigger:"#page6 #part-1",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page6 #part-2 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     x:300,
+     scrollTrigger:{
+         trigger:"#page6 #part-2",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+            
+gsap.from("#page7 #page ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page7 #page",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+              
+gsap.from("#page8 .blog ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page8 .blog",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+                
+gsap.from("#page9 #first ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page9 #first",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+                
+gsap.from("#page9 #second ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     y:200,
+     scrollTrigger:{
+         trigger:"#page9 #second",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+                
+gsap.from("#page9 #part2 ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     x:200,
+     scrollTrigger:{
+         trigger:"#page9 #part2",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+                
+gsap.from("#meet ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     x:450,
+     scrollTrigger:{
+         trigger:"#meet",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+   
+                
+gsap.from("#page11 .vapers ",{
+    opacity:0,
+     duration:2,
+     dealy:1,
+     x:-450,
+     scrollTrigger:{
+         trigger:"#page11 .vapers",
+         scroller:"body",
+        //  markers:true,
+         start:"top 70%",
+         end :"bottom 10%",
+     } })
+   
+   
+   
+   
+   
